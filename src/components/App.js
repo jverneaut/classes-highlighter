df4094a694abb0266517fd9d18698cc7e064e7c4
Julien Verneaut <jverneaut@gmail.com>
2021-01-02 17:46:40 +0100

    Setup extension

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <h1>Hi, Chrome Extension</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    popup: ['./src/popup.js', './src/styles/popup.scss'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['popup'],
+      filename: 'popup.html',
+      template: './src/popup.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/manifest.json' }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
